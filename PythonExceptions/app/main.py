f9ef01bd7afbd1d2eb4fa8324c0f037ac6225476
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-05-29 11:42:59 -0400

    exceptions, handling, and chaining except blocks

---
+++ PythonExceptions/app/main.py
@@ -0,0 +1,48 @@
+# Let's start by raising an Exception on purpose and see what happens
+# result = 5 / 0
+
+# We can also raise Exceptions OURSELVES as a way to validate user behaviors
+def print_money(amount):
+    if amount < 0:
+        raise ValueError("Amount can't be negative!")
+
+    print(f"You printed ${amount}!")
+
+print_money(1)
+
+# This time, we won't let the runtime stop. We'll HANDLE the Exception!
+try:
+    print_money(-1)
+except ValueError as e: # Exceptions are just objects! We can store them in variables!
+    print(f"Error occured! {e}")
+finally:
+    # The finally block always runs -
+    # typically used for stuff like cleanup (closing files, DB connections, etc)
+    print("I'm always gonna run no matter what")
+
+print("I DO run, because we handled the Exception instead of letting the app crash")
+
+
+# We can also CHAIN exceptions if multiple things could go wrong
+# We can have as many except blocks as we want! To account for different problems
+# The first Except block that matches the problem will run
+    # Read below on why this is technically bad code:
+def divide(a, b):
+    try:
+        print(a/b)
+    except ArithmeticError:
+        print("Something mathematical went wrong ")
+    except ZeroDivisionError:
+        print("Can't divide by zero!")
+    except Exception:
+        print("This block would have caught any exception at all")
+    # We could have had a finally block here, but it's optional
+
+divide(5, 0)
+
+"""
+MAKE SURE TO PUT THE MORE SPECIFIC EXCEPT BLOCKS FIRST!
+
+In the try/except above, the more generic ArithmeticError gets caught
+But we could have been more clear to the user, who's real problem is that they divided by zero
+"""
