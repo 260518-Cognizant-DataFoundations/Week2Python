f86121773ca58e5bf5824f7dc52db321c7c90caa
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-01 10:27:50 -0400

    initial commit + comment

---
+++ PythonModules/app/main.py
@@ -0,0 +1,12 @@
+"""
+We'll have two different classes in this app -
+
+1) AirFryer - Has basic to somewhat complex methods that we'll use to:
+    -See some modules in action (math, logging, regex)
+    -write some tests with the Pytest module
+    -TODO: If possible, time permitting, we'll use the JSON and pylint modules as well
+
+2) AirFryerTests - A Test Suite where we'll write Pytest tests for AirFryer's methods
+
+We'll call the AirFryer methods below for fun (and to see our logs at work)
+"""
