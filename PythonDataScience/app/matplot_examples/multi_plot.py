91016c2d9e61f199dfa3ce9698725e59ee498288
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-02 12:11:18 -0400

    months in multi plot

---
+++ PythonDataScience/app/matplot_examples/multi_plot.py
@@ -0,0 +1,6 @@
+# We can easily show multiple lines in one line chart
+# It's as simple as calling multiple plot() functions before show()
+
+import matplotlib.pyplot as plt
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
