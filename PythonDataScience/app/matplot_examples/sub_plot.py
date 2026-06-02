e39b874658eccad300a1dc7260fd886ab1217989
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-02 15:10:27 -0400

    setup for subplot

--- PythonDataScience/app/matplot_examples/sub_plot.py
+++ PythonDataScience/app/matplot_examples/sub_plot.py
@@ -1,3 +1,7 @@
 # subplots - multiple charts in a single figure
 
 import matplotlib.pyplot as plt
+
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May']
+sales = [10, 20, 30, 40, 50]
+returns = [1, 2, 3, 4, 5]
