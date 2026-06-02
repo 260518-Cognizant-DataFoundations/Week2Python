564a6c20eacf776ac75a3252b3e703340d372204
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-02 10:23:53 -0400

    initial commit and numpy comment

---
+++ PythonDataScience/app/numpy_example.py
@@ -0,0 +1,10 @@
+"""
+Here's our first look at "Why NumPy?" - SPEED!
+NumPy performs operations on entire datasets at once, without loops.
+This superpower is called "vectorization"
+
+This cleans up your code, and makes your operations significantly faster.
+The bigger the dataset, the more dramatic the difference.
+
+Also, we're importing the time module to measure how long our operations take
+"""
