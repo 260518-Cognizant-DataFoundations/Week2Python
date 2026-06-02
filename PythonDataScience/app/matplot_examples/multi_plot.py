039b6e799c87970afac966c8badd70e302e02877
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-02 12:13:15 -0400

    months in multi plot

--- PythonDataScience/app/matplot_examples/multi_plot.py
+++ PythonDataScience/app/matplot_examples/multi_plot.py
@@ -4,3 +4,6 @@
 import matplotlib.pyplot as plt
 
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+air_fryers = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650] # upward trend
+toasters = [650, 600, 550, 500, 450, 400, 350, 300, 250, 200, 150, 100] # downward trend
+blenders = [300, 320, 310, 330, 290, 350, 370, 360, 340, 380, 390, 410] # mixed bag
