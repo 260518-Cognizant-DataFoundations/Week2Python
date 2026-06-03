a02dc9a9041b979cff6eb0c99c5a423e52ec7200
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-06-03 10:23:55 -0400

    comment

---
+++ PythonDataScience/app/pandas_numpy_matplotlib.py
@@ -0,0 +1,9 @@
+"""
+Ok we had lots of fun playing around with hardcoded data
+
+But let's use a "real" .csv dataset and use all 3 technologies we've learned
+
+We'll start by finally using a Pandas dataframe to load and manipulate the real data
+Then use Numpy to do some calculations to use in our chart
+And visualize the data with Matplotlib!
+"""
